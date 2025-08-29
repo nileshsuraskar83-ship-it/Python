a93094fb3da15190819f6c80f26c6592275690a6
nileshsuraskar83-ship-it <nileshsuraskar83@gmail.com>
2025-08-29 15:11:45 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+#Addition of Two Numbers
+a=int(input("Enter the First Number:"))
+b=int(input("Enter the Second Number:"))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+
+#Concatenation of name
+f_name=input("Enter your first name:")
+l_name=input("Enter your last name:")
+full_name=f_name+" "+l_name
+print("Hello,",full_name+"!Welcome to the Python Program.")
